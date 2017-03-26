b996de96032c76e64d98a76262094121dc0fea9f
Cesar Cruz <cesarcruz91@gmail.com>
2017-03-26 18:53:17 -0400

    Added csv parsing

--- controllers/index.js
+++ controllers/index.js
@@ -3,27 +3,19 @@ const User = require('../models/user.js')
 const Data = require('../models/data.js')
 const passport = require('passport')
 const multer = require('multer')
+const path = require('path')
+const Baby = require('babyparse')
+const fs = require('fs')
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './my-uploads')
-    },
+    destination: path.resolve(__dirname, '../tmp/uploads'),
     filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, `${file.fieldname}-${Date.now()}.csv`)
     }
 })
+
 const upload = multer({
     storage: storage
 })
-const Transform = require('stream-transform')
-const Converter = require('csvtojson').Converter
-const csv = require('csvtojson')
-const generate = require('csv-generate');
-const parse = require('csv-parse')
-const stringify = require('csv-stringify')
-const output = []
-const data = []
-const should = require('should')
-
 
 
 function protect(req, res, next) {
@@ -96,148 +88,11 @@ router.get('/upload', protect, function(req, res) {
 //Upload functionality
 //req.file -> is going to represent the uploaded File buffer
 router.post('/upload', upload.single('data'), function(req, res) {
-    console.log(req.file)
+    let parsedData = Baby.parseFiles(req.file.path, {
+        header: true
+    })
+    console.log(parsedData)
+    res.send(200)
 })
-
-//CSV to JSON
-//generating csv strings & JS objects
-let generator = generate()
-generator.on('readable', function() {
-    while (d = generator.read()) {
-        data.push(d)
-    }
-})
-generator.on('error', function(err) {
-    console.log(err);
-})
-generator.on('end', function() {
-    data.should.eql()
-})
-
-//Parsing CSV
-let parser = parse({ delimiter: ':' })
-
-parser.on('readable', function() {
-    while (record = parser.read()) {
-        output.push(record)
-    }
-})
-parser.on('error', function(err) {
-    console.log(err)
-})
-parser.on('finish', function() {
-    output.should.eql()
-
-})
-parser.write()
-parser.write()
-parser.end()
-
-//Transform CSV
-let transformer = Transform(function(data) {
-    data.push(data.shift())
-    return data
-});
-transformer.on('readable', function() {
-    while (row = transformer.read()) {
-        output.push(row)
-    }
-})
-transformer.on('error', function(err) {
-    console.log(err.message)
-})
-transformer.on('finish', function() {
-    output.should.eql([])
-})
-transformer.write()
-transformer.write()
-transformer.end()
-
-//Stringyfy CSV
-let stringData = ''
-stringifier = stringify({ delimiter: ':' })
-stringifier.on('readable', function() {
-    while (row = stringifier.read()) {
-        stringData += row
-    }
-})
-stringifier.on('error', function(err) {
-    console.log(err.message)
-})
-stringifier.on('finish', function() {
-    stringData.should.eql()
-})
-stringifier.write()
-stringifier.write()
-stringifier.end()
-
-
-
-
 
 module.exports = router
-
-// let converter = csv()
-// csv()
-//     converter.fromStream(csvReadStream)
-//     converter.on('csv', (csvRow, rowIndex) => {
-//         // csvRow is an array
-//         //rowIndex is the row number of csv line
-//     })
-//     converter.on('data', (data) => {
-//         //data is a buffer getObject
-//         const jsonStr = data.toString('utf8')
-//     })
-//     converter.on('error', (err) => {
-//         console.log(err)
-//     })
-//     converter.on('done', (error) => {
-//         console.log('Parsing complete!')
-//     })
-
-
-
-
-//function -uploads CSV files to AWS S3 bucket
-// const AWS = require('aws-sdk')
-//     AWS.config.update({
-//         accessKeyId: 'AKIAIZUPD5IITORJVAHQ',
-//         secretAccessKey: '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',
-//         region: 'us-east-1'
-//     })
-
-//     let s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-//     let myBucket = 'wad-finalproject-app-uploads'
-//     let fileName = 'req.file'
-
-//     s3.createBucket({
-//         Bucket: myBucket
-//     }, function(err, data) {
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             params = {
-//                 Bucket: myBucket,
-//                 Key: fileName,
-
-//             }
-//         }
-//         s3.putObject(params, function(err, data) {
-//             if (err) {
-//                 console.log(err)
-//             } else {
-//                 console.log("Successfully uploaded data to myBucket")
-//             }
-//         })
-
-//         s3.getObject(params, function(err, data) {
-//             if (err) {
-//                 console.log(err)
-//             }
-//             res.json({
-//                 signed_request: data,
-//                 url: 'http://' + myBucket + '.s3.amazonaws.com/' + fileName
-//             })
-//         })
-//     })
-// })
